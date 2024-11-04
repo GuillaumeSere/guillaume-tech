c38f2d2582f1b01c1824da2ee53d4791b65cb955
Guillaume Sere <guillaumesere@hotmail.fr>
2024-11-04 15:35:57 +0100

    modif home page

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import './service.css';
 import github from '../assets/github.png';
 import langages from '../assets/langages.jpg';
+import img1 from '../assets/course-1.svg';
+import img2 from '../assets/course-3.svg';
 
 const Service = () => {
     return (
         <div className='service'>
-            <h1 className='title-service'>Sur <span>Beauvais</span> appelez vite !</h1>
             <div className="text">
-                <span className='number'>📱 06.60.40.09.24</span>
-                <span className='number'><a className='link' href="mailto:guillaumesere60@gmail.com" target="_blank" rel="noreferrer">📧 guillaumesere60@gmail.com</a></span>
-                <h3>👍<u className='desc'>Services Proposés</u> : </h3>
+            <h1 className='title-service'>Sur <span>Beauvais</span> appelez vite !</h1>
+            <span className='number'>📱 06.60.40.09.24</span>
+            <span className='number'><a className='link' href="mailto:guillaumesere60@gmail.com" target="_blank" rel="noreferrer">📧 guillaumesere60@gmail.com</a></span>
+            </div>
+            <div className="box-image">
+            <img src={img1} className='img-deco' alt="" />
+            <img src={img2} className='img-deco' alt="" />
+            </div>
+            <div className="text">
+                <h2>👍<u className='desc'>Services Proposés</u> : </h2>
                 <p> 🖥️ Formation de base en informatique (utilisation des ordinateurs, tablettes et smartphones) 💻<br />
                     📑 Assistance pour les démarches administratives en ligne (création de comptes divers avec sécurisation...) ⌨️<br />
                     👨‍💻 Aide à l'utilisation des outils de communication, réseaux sociaux, logiciels d'accessibilité pour les personnes malvoyantes, ainsi que l'intelligence artificielle 👩‍💻<br />
