f5791ff83a580ebb8ed5fa0a37637a8eda8ca5cc
Guillaume Sere <guillaumesere@hotmail.fr>
2025-06-13 15:45:27 +0200

    modification site et ajout vidéo

--- src/components/Tarif.jsx
+++ src/components/Tarif.jsx
@@ -37,21 +37,21 @@ const Tarif = () => {
                     <p>Introduction au codage <span>(10 minutes) </span>
                         Qu'est-ce que le codage et pourquoi est-il important ?
                         Aperçu du cours et de ce que vous apprendrez.
-                        Configurer votre environnement de codage <span>(20 minutes)</span>.
+                        Configurer votre environnement de codage <span>(20 minutes)</span>
                         Installer un éditeur (par exemple Visual Studio Code, Sublime Text, Atom).
                         Configurer votre espace de travail (par exemple, configurer la coloration syntaxique, les thèmes).
                         Introduction à la ligne de commande et aux commandes de base.
-                        Fondamentaux de la programmation <span>(40 minutes)</span>.
+                        Fondamentaux de la programmation <span>(40 minutes)</span>
                         Introduction aux langages de programmation (par exemple, JavaScript, HTML/CSS).
                         Types de données (par exemple, nombres, chaînes, booléens).
                         Variables et affectation
                         Opérateurs (par exemple arithmétiques, de comparaison, logiques).
                         Structures de contrôle (par exemple instructions if/else, boucles).
                         Les fonctions
-                        Exercice pratique de codage <span>(30 minutes)</span>.
+                        Exercice pratique de codage <span>(30 minutes)</span>
                         Écrivez et exécutez votre propre code en utilisant les concepts appris dans le cours.
                         Dépannez et déboguez votre code.
-                        Conclusion et prochaines étapes <span>(10 minutes)</span>.
+                        Conclusion et prochaines étapes <span>(10 minutes)</span>
                         Récapitulatif de ce que vous avez appris.
                         Conseils pour continuer à apprendre et à améliorer vos compétences en codage
                         Ressources pour un apprentissage et une pratique plus poussés</p>
