cf23490027cfde09c4f2f8e8df4ffe1136f2b923
Guillaume Sere <guillaumesere@hotmail.fr>
2025-08-05 13:41:52 +0200

    modif logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./header.css"
-import logo from '../../assets/images/logo1.png'
+import logo1 from '../../assets/images/logo1.png'
+import logo2 from '../../assets/images/logo2.png'
+import { useTheme } from '../../context/ThemeContext';
 
 const Header = () => {
+      const { isDarkMode } = useTheme();
+
     return (
         <div className='header'>
-            <img className='logo' src={logo} alt="logo" />
+            <img className='logo' src={isDarkMode ? logo1 : logo2} alt="logo" />
             <div className="description">
                 <h1 className="title">Guillaume <span>Tech</span></h1>
                 <h2 className="sub-title"><u className='desc'>Assistance Informatique à la Personne</u></h2>
